8fd64d9422ef1b2bb8fe5b12d59d022ad5338372
Graham Markall <graham.markall@continuum.io>
2015-07-22 14:16:56 +0100

    Document utils class

--- wot/utils.py
+++ wot/utils.py
@@ -1,8 +1,17 @@
 import gnupg
 
 class GPG(gnupg.GPG):
+    '''
+    GPG class with additional methods to be slightly more convenient for
+    exploring a web of trust.
+    '''
 
     def list_keys(self, *, secret=False, keyid=None):
+        '''
+        List the keys currently in the keyring. Returns a list of the keys,
+        or a single key if keyid is specified.
+        '''
+
         keys = super(GPG, self).list_keys(secret=secret)
 
         if keyid is not None:
@@ -15,6 +24,10 @@ class GPG(gnupg.GPG):
         return keys
 
     def list_sigs_by_keyid(self, keyid):
+        '''
+        Return a list of the keyids of the keys that a key with
+        keyid is signed by.
+        '''
         # list_sigs takes multiple keys to return signatures for, so we need
         # to take the first result.
         sig_info = self.list_sigs(keyid)[0]
@@ -23,5 +36,8 @@ class GPG(gnupg.GPG):
         return sig_keyids
 
     def get_uids(self, keyid):
+        '''
+        Return a list of the uids of the key with keyid
+        '''
         key = self.list_keys(keyid=keyid)
         return key['uids']
