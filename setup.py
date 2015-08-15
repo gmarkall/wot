93c702eb282fd00dce100915314d3967b28786b6
Graham Markall <graham.markall@continuum.io>
2015-08-15 19:49:02 +0100

    Fix linkage

--- setup.py
+++ setup.py
@@ -5,19 +5,20 @@ packages = [ 'wot' ]
 
 gpgme_cflags = subprocess.check_output(['gpgme-config', '--cflags'],
                                        universal_newlines=True)
+
 gpgme_ldflags = subprocess.check_output(['gpgme-config', '--libs'],
                                         universal_newlines=True)
-
-print(gpgme_cflags.strip())
-print(gpgme_ldflags.strip())
+split_ldflags = gpgme_ldflags.strip().split()
+library_dirs = [ arg[2:] for arg in split_ldflags if arg[:2] == '-L' ]
+libraries = [ arg[2:] for arg in split_ldflags if arg[:2] == '-l' ]
 
 CFLAGS = [ gpgme_cflags.strip() ]
-LDFLAGS = [ gpgme_ldflags.strip() ]
 
 ext_gpgme = Extension(name='wot._gpgme',
                       sources=['wot/_gpgme.c'],
                       extra_compile_args=CFLAGS,
-                      extra_link_args=LDFLAGS)
+                      libraries=libraries,
+                      library_dirs=library_dirs)
 
 ext_modules = [ ext_gpgme ]
 
