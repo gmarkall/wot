c588fdc350189f30a9b14eccaf9eb609997e1e96
Graham Markall <graham.markall@continuum.io>
2015-10-31 17:04:31 +0000

    Begin adding to_dict to Keyring

--- wot/gpgme.py
+++ wot/gpgme.py
@@ -11,3 +11,21 @@ if _gpgme_version != '1.5.5':
 class Keyring(_gpgme.Keyring):
     def __init__(self, keyring_path):
         super().__init__(keyring_path)
+        self._dict = {'keyid': [], 'signedby': [], uid: [], 'timestamp': []}
+
+    def to_dict(self):
+        '''
+        Convert to a dict. The dict contains lists of the key IDs, the lists of
+        key IDs signing the key, the uid, and the timestamp.
+
+        FIXME: Signatures are not yet shown.
+        FIXME: Since importing is not yet implemented, timestamps will all be
+               the same (the time of the first call).
+        '''
+        keys = self.list_keys()
+        new_keys = set(self._dict['keyid']
+        ### Got this far then realised that the list_keys operation takes keyids
+        ### and returns uids, which is not ideal for this function.
+
+
+
